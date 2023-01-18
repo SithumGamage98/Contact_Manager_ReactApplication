eb90517a38d76bd1461f5b6b955e366af417a89e
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-01-18 18:32:00 +0530

    Update App.js and Add_contacts.js , Added react hooks parts and useState pats

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import React, {useState , useEffect} from 'react';
 import './App.css';
 import Header from './components/Header';
 import AddContact from './components/Add_Contact';
 import ContactList from './components/ContactList'
 import ContactCard from './components/Contact_Card';
 
-function App() {
+//--Why useEffect -> if the value is changed, use effect can re-render the component
+//That mean useEffect can save data, when the component refresh--> can store some data in the LOCAL STORAGES
+//Use effects simply use to -> STORING AND RETRIVING DATA
 
+function App() {
+ 
+  //Method 01---using static array for get Data
+  /*
   //create an array list for contact details
   const contacts = [
       {
@@ -21,11 +27,47 @@ function App() {
         email : "Gayan@123gmail.com"
       }
   ]
+  */
+
+  //Method 02 ---- using React hooks - > useState
+  //Create a state for contacts (Contact details)
+
+  //Key for LOCAL STORAGE
+  const LOCAL_STORAGE_KEY = "constracts";
+
+  const [contacts , setContacts] = useState([]);
+
+  const addContacthandler = (contact)=>{
+
+    console.log(contact);
+    //this for display entered data
+    setContacts([...contacts,contact])
+
+  }  
   
+  
+  //Another useEffect for get/retrive data from the LOCAT STORAGE
+  useEffect(()=>{
+
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retriveContacts) setContacts(retriveContacts)
+  },[contacts]);
+
+  //useEffect for save data in the LOCAT STORAGE
+
+  useEffect(()=>{
+
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+
+  },[contacts]);
+
+
+
+
   return (
     <div className='ui container'>
       <Header/>
-      <AddContact/>
+      <AddContact addContacthandler={addContacthandler}/>
       <ContactList x={contacts} />
     </div>
   );
--- src/components/Add_Contact.js
+++ src/components/Add_Contact.js
@@ -2,20 +2,51 @@ import React from "react";
 
 
 class AddContact extends React.Component{
+
+    //Create state
+    state = {
+
+        name : '',
+        email : '',
+
+    }
+
+    //Add function onSubmit={this.add}
+
+    add=(e)=>{
+         e.preventDefault();
+        if(this.state.name==="" && this.state.email===""){
+          
+            alert("Plz Fill all the fileds")
+            return;
+        }
+
+        this.props.addContacthandler(this.state);
+        console.log(this.state);
+        //Set state ------After adding details what should be happen?
+        this.setState({
+
+            name : "",
+            email : ""
+
+
+        })
+    }
+
     render(){
 
         return(
         <div className="ui main">
             <h2>Add Contacts</h2>
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.add}>
                 <div className="field">
                     <label>Name</label>
-                    <input type ="text" name="name" placeholder="Enter name" />
+                    <input type ="text" name="name" placeholder="Enter name" value={this.state.name} onChange={(e)=>this.setState({name:e.target.value})} />
                 </div>
 
                 <div className="field">
                     <label>Email</label>
-                    <input type ="text" name="email" placeholder="Enter Emil" />
+                    <input type ="text" name="email" placeholder="Enter Emil" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})} />
                 </div>
 
                 <div>
